eb2c2b463910343afe1409a3ac2aadc322e7d3e0
Szymon <szymonsetzke98@gmail.com>
2020-05-04 18:44:52 +0200

    Zadanie 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal dog = new Animal("dog");
+        dog.name = "Dino";
+
+        dog.feed();
+
+        for (int i = 0; i < 12; i++) {
+            dog.Walk();
+        }
+        dog.feed();
     }
 }
