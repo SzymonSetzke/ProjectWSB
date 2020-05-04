dfbc983002f4049e833b9d603f136a9d7f15c5c9
Szymon <szymonsetzke98@gmail.com>
2020-05-04 19:04:40 +0200

    Zadanie 2

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class Human {
+
+    Animal pet;
+    Car dreamcar;
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Human me = new Human();
+
         Animal dog = new Animal("dog");
         dog.name = "Dino";
 
@@ -12,5 +14,9 @@ public class Main {
             dog.Walk();
         }
         dog.feed();
+
+        Car dreamcar = new Car("VW", "Passat", 1998);
+        me.dreamcar = dreamcar;
+
     }
 }
