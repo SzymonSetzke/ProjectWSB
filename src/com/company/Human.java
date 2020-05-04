8e88fa6b46684a6781e4265940dcb84ae442f832
Szymon <szymonsetzke98@gmail.com>
2020-05-04 21:22:27 +0200

    Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,8 +1,20 @@
 package com.company;
-
 public class Human {
-
+    String firstName;
+    String lastName;
+    private Double salary;
+    public Double getSalary() {
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        if(salary >= 0) {
+            System.out.println("Your salary is now: " + salary);
+            System.out.println("New value of salary has been added to salary history.");
+            System.out.println("Please pick up the annex to the contract form Mrs Hania.");
+            System.out.println("ZUS and US has been informed.");
+            this.salary = salary;
+        }
+    }
     Animal pet;
-    Car dreamcar;
-
+    Car car;
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,19 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
         Human me = new Human();
-
+        me.firstName = "Szymon";
+        me.lastName = "Setzke";
         Animal dog = new Animal("dog");
         dog.name = "Dino";
-
         dog.feed();
-
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 20; i++) {
             dog.Walk();
         }
         dog.feed();
-
+        dog.feed();
         Car dreamcar = new Car("VW", "Passat", 1998);
-        me.dreamcar = dreamcar;
-
+        me.car = dreamcar;
+        me.setSalary(1.0);
     }
 }
